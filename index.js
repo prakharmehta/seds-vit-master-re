db474a8f454f9209b961a4515f505add6db70847
prakharmehta <prakh.mehta@gmail.com>
2020-09-27 00:20:42 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+var path = require('path')
+require("ejs");
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(process.env.PORT || 3000);
