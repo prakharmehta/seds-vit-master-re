b80767b0a2641e5303c82e5a227c1eac9ed197dc
Tushar S Agrawal <tusharsagrawal16@gmail.com>
2021-11-17 23:40:55 +0530

    centered text

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get("/board", (_, res) => {
   res.render("board");
 });
 app.get("/aphelion", (_, res) => {
-  res.send("<h1 style=`text-align: center`>The registrations are yet to open </h1>");
+  res.send("<h1 style='text-align:center;'>The registrations are yet to open </h1>");
 });
 
 app.listen(process.env.PORT || 3000);
