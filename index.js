1d72e49975602572c7065e591467721c66583f1b
Tushar S Agrawal <tusharsagrawal16@gmail.com>
2021-11-23 00:30:14 +0530

    test commit

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.get("/aphelion", (_, res) => {
   res.redirect("https://forms.gle/puwy2NR6kvHmEKQy7");
 });
 app.get("/testd", (_, res) => {
-  if (Date.now() > 1637650320000) res.download("./public/pdf/serve.pdf");
-  else res.send("Problem releases at " + Date.now() + " ||||| " + 1637650320000);
+  if (Date.now() > 1637607900000) res.download("./public/pdf/serve.pdf");
+  else res.send("Problem releases at " + Date.now() + " ||||| " + 1637607900000);
 });
 
 app.listen(process.env.PORT || 3000);
