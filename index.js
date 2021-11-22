71727dcb1f14f50b76d66aba2b73a2323ce335b7
Tushar S Agrawal <tusharsagrawal16@gmail.com>
2021-11-23 00:08:34 +0530

    pdf with time

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ app.get("/board", (_, res) => {
 app.get("/aphelion", (_, res) => {
   res.redirect("https://forms.gle/puwy2NR6kvHmEKQy7");
 });
-// app.get("/testd", (_, res) => {
-//   res.download("./public/pdf/serve.pdf");
-// });
+app.get("/testd", (_, res) => {
+  if (Date.now() > 1637649600000) res.download("./public/pdf/serve.pdf");
+  else res.send("Problem releases at 12:10am");
+});
 
 app.listen(process.env.PORT || 3000);
