2b879b00d8d04d80e74ab0dce39317899c8a8b9a
Tushar S Agrawal <tusharsagrawal16@gmail.com>
2021-11-26 21:02:51 +0530

    linked disabled

--- index.js
+++ index.js
@@ -15,16 +15,12 @@ app.get("/board", (_, res) => {
   res.render("board");
 });
 app.get("/aphelion", (_, res) => {
-  if (Date.now() > 1637854200000) res.download("./public/pdf/aphelion_problem_statements.pdf");
-  else res.send("<h1>The problem statements will be released at 9pm.</h1>");
+  // if (Date.now() > 1637854200000) res.download("./public/pdf/aphelion_problem_statements.pdf");
+  res.send("<h1>The Ideathon has ended.</h1>");
 });
 
 app.get("/aphelion/submission", (_, res) => {
-  if (Date.now() > 1637854200000) res.redirect("https://forms.gle/5BU6bjyDUPSdmzJr5");
-  else
-    res.send(
-      "<h1>The submission form will be live at 9pm tonight. Be sure to submit before 9pm tomorrow (26th November). </h1>"
-    );
+  res.send("<h1>We are no longer accepting solutions, thank you for participation.</h1>");
 });
 
 // app.get("/testd", (_, res) => {
