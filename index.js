cfdfc4e07ce33c5caae1ae37481843e1265553aa
Tushar S Agrawal <tusharsagrawal16@gmail.com>
2022-01-04 00:09:10 +0530

    board update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 var path = require("path");
+const members = require("./boardMembers.json");
 require("ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -12,12 +13,8 @@ app.get("/", (_, res) => {
 });
 
 app.get("/board", (_, res) => {
-  res.render("board");
+  res.render("board", { members });
 });
 
-// app.get("/testd", (_, res) => {
-//   if (Date.now() > 1637607900000) res.download("./public/pdf/serve.pdf");
-//   else res.send("Problem releases at " + Date.now() + " ||||| " + 1637607900000);
-// });
 
 app.listen(process.env.PORT || 3000);
--- public/scripts/carousel.js
+++ public/scripts/carousel.js
@@ -1,122 +1,128 @@
-let prakharQuote1 = document.querySelector('#member14 .board__card__marketing__profile__info__quote__text div');
-prakharQuote1.innerHTML = `<span>Life is simple. Our</span><div style="transform: rotate(180deg);">PERSPECTIVE</div><span>isn't!</span>`;
-let prakharQuote2 = document.querySelector('.board__members--mobile .slider .wrapper .slides .slide #member14  .board__card__marketing__profile__info__quote__text div')
-prakharQuote2.innerHTML = `<span>Life is simple. Our</span><div style="transform: rotate(180deg);">PERSPECTIVE</div><span>isn't!</span>`;
-var slider = document.getElementById('slider'),
-    sliderItems = document.getElementById('slides'),
-    prev = document.getElementById('prev'),
-    next = document.getElementById('next');
+// let prakharQuote1 = document.querySelector('#member14 .board__card__marketing__profile__info__quote__text div');
+// prakharQuote1.innerHTML = `<span>Life is simple. Our</span><div style="transform: rotate(180deg);">PERSPECTIVE</div><span>isn't!</span>`;
+// let prakharQuote2 = document.querySelector('.board__members--mobile .slider .wrapper .slides .slide #member14  .board__card__marketing__profile__info__quote__text div')
+// prakharQuote2.innerHTML = `<span>Life is simple. Our</span><div style="transform: rotate(180deg);">PERSPECTIVE</div><span>isn't!</span>`;
+var slider = document.getElementById("slider"),
+  sliderItems = document.getElementById("slides"),
+  prev = document.getElementById("prev"),
+  next = document.getElementById("next");
 
 function slide(wrapper, items, prev, next) {
-    var posX1 = 0,
-        posX2 = 0,
-        posInitial,
-        posFinal,
-        threshold = 100,
-        slides = items.getElementsByClassName('slide'),
-        slidesLength = slides.length,
-        slideSize = items.getElementsByClassName('slide')[0].offsetWidth,
-        firstSlide = slides[0],
-        lastSlide = slides[slidesLength - 1],
-        cloneFirst = firstSlide.cloneNode(true),
-        cloneLast = lastSlide.cloneNode(true),
-        index = 0,
-        allowShift = true;
+  var posX1 = 0,
+    posX2 = 0,
+    posInitial,
+    posFinal,
+    threshold = 100,
+    slides = items.getElementsByClassName("slide"),
+    slidesLength = slides.length,
+    slideSize = items.getElementsByClassName("slide")[0].offsetWidth,
+    firstSlide = slides[0],
+    lastSlide = slides[slidesLength - 1],
+    cloneFirst = firstSlide.cloneNode(true),
+    cloneLast = lastSlide.cloneNode(true),
+    index = 0,
+    allowShift = true;
 
-    // Clone first and last slide
-    items.appendChild(cloneFirst);
-    items.insertBefore(cloneLast, firstSlide);
-    wrapper.classList.add('loaded');
+  // Clone first and last slide
+  items.appendChild(cloneFirst);
+  items.insertBefore(cloneLast, firstSlide);
+  wrapper.classList.add("loaded");
 
-    // Mouse events
-    items.onmousedown = dragStart;
+  // Mouse events
+  items.onmousedown = dragStart;
 
-    // Touch events
-    items.addEventListener('touchstart', dragStart);
-    items.addEventListener('touchend', dragEnd);
-    items.addEventListener('touchmove', dragAction);
+  // Touch events
+  items.addEventListener("touchstart", dragStart);
+  items.addEventListener("touchend", dragEnd);
+  items.addEventListener("touchmove", dragAction);
 
-    // Click events
-    prev.addEventListener('click', function () { shiftSlide(-1) });
-    next.addEventListener('click', function () { shiftSlide(1) });
+  // Click events
+  prev.addEventListener("click", function () {
+    shiftSlide(-1);
+  });
+  next.addEventListener("click", function () {
+    shiftSlide(1);
+  });
 
-    // Transition events
-    items.addEventListener('transitionend', checkIndex);
+  // Transition events
+  items.addEventListener("transitionend", checkIndex);
 
-    function dragStart(e) {
-        e = e || window.event;
-        e.preventDefault();
+  function dragStart(e) {
+    e = e || window.event;
+    e.preventDefault();
+    posInitial = items.offsetLeft;
+
+    if (e.type == "touchstart") {
+      posX1 = e.touches[0].clientX;
+    } else {
+      posX1 = e.clientX;
+      document.onmouseup = dragEnd;
+      document.onmousemove = dragAction;
+    }
+  }
+
+  function dragAction(e) {
+    e = e || window.event;
+
+    if (e.type == "touchmove") {
+      posX2 = posX1 - e.touches[0].clientX;
+      posX1 = e.touches[0].clientX;
+    } else {
+      posX2 = posX1 - e.clientX;
+      posX1 = e.clientX;
+    }
+    items.style.left = items.offsetLeft - posX2 + "px";
+  }
+
+  function dragEnd(e) {
+    posFinal = items.offsetLeft;
+    if (posFinal - posInitial < -threshold) {
+      shiftSlide(1, "drag");
+    } else if (posFinal - posInitial > threshold) {
+      shiftSlide(-1, "drag");
+    } else {
+      items.style.left = posInitial + "px";
+    }
+
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+
+  function shiftSlide(dir, action) {
+    items.classList.add("shifting");
+
+    if (allowShift) {
+      if (!action) {
         posInitial = items.offsetLeft;
+      }
 
-        if (e.type == 'touchstart') {
-            posX1 = e.touches[0].clientX;
-        } else {
-            posX1 = e.clientX;
-            document.onmouseup = dragEnd;
-            document.onmousemove = dragAction;
-        }
+      if (dir == 1) {
+        items.style.left = posInitial - slideSize + "px";
+        index++;
+      } else if (dir == -1) {
+        items.style.left = posInitial + slideSize + "px";
+        index--;
+      }
     }
 
-    function dragAction(e) {
-        e = e || window.event;
+    allowShift = false;
+  }
 
-        if (e.type == 'touchmove') {
-            posX2 = posX1 - e.touches[0].clientX;
-            posX1 = e.touches[0].clientX;
-        } else {
-            posX2 = posX1 - e.clientX;
-            posX1 = e.clientX;
-        }
-        items.style.left = (items.offsetLeft - posX2) + "px";
+  function checkIndex() {
+    items.classList.remove("shifting");
+
+    if (index == -1) {
+      items.style.left = -(slidesLength * slideSize) + "px";
+      index = slidesLength - 1;
     }
 
-    function dragEnd(e) {
-        posFinal = items.offsetLeft;
-        if (posFinal - posInitial < -threshold) {
-            shiftSlide(1, 'drag');
-        } else if (posFinal - posInitial > threshold) {
-            shiftSlide(-1, 'drag');
-        } else {
-            items.style.left = (posInitial) + "px";
-        }
-
-        document.onmouseup = null;
-        document.onmousemove = null;
+    if (index == slidesLength) {
+      items.style.left = -(1 * slideSize) + "px";
+      index = 0;
     }
 
-    function shiftSlide(dir, action) {
-        items.classList.add('shifting');
-
-        if (allowShift) {
-            if (!action) { posInitial = items.offsetLeft; }
-
-            if (dir == 1) {
-                items.style.left = (posInitial - slideSize) + "px";
-                index++;
-            } else if (dir == -1) {
-                items.style.left = (posInitial + slideSize) + "px";
-                index--;
-            }
-        };
-
-        allowShift = false;
-    }
-
-    function checkIndex() {
-        items.classList.remove('shifting');
-
-        if (index == -1) {
-            items.style.left = -(slidesLength * slideSize) + "px";
-            index = slidesLength - 1;
-        }
-
-        if (index == slidesLength) {
-            items.style.left = -(1 * slideSize) + "px";
-            index = 0;
-        }
-
-        allowShift = true;
-    }
+    allowShift = true;
+  }
 }
 
 slide(slider, sliderItems, prev, next);
