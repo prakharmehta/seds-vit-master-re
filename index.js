c7fb533a28595d8cf10cf382c90a94d55b1d19f2
Tushar S Agrawal <tusharsagrawal16@gmail.com>
2022-01-10 01:22:33 +0530

    basic event done

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express();
 var path = require("path");
 const members = require("./boardMembers.json");
 const sanityConfig = require("./sanity_config.json");
+
+const sanityClient = require("@sanity/client");
+const imageUrlBuilder = require("@sanity/image-url");
 require("ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
-const sanityClient = require("@sanity/client");
 const client = sanityClient({
   projectId: "wvxxjfa8",
   dataset: "production",
@@ -16,6 +18,8 @@ const client = sanityClient({
   token: sanityConfig.token, // or leave blank for unauthenticated usage
   useCdn: false, // `false` if you want to ensure fresh data
 });
+
+const imageBuilder = imageUrlBuilder(client);
 
 // Routes
 app.get("/", (_, res) => {
@@ -26,15 +30,20 @@ app.get("/board", (_, res) => {
   res.render("board", { members });
 });
 
-app.get("/*", (_, res) => {
+app.get("/*", async (_, res) => {
   const query = `*[_type == "event" && eventEndDate.utc > $today]`;
   const today = new Date("2022-01-12").toISOString();
-  // console.log(today);
-  // client.fetch(query, { today }).then((event) => {
-  //   console.log(event);
-  // });
+  const event = await client.fetch(query, { today });
+  console.log(event);
+  const { title, eventDate, poster, path, eventDescription = "", tagLine = "" } = event[0];
+  posterUrl = imageBuilder.image(poster).url();
   res.render("event", {
-    eventDate: today,
+    eventDate: eventDate.utc,
+    title,
+    eventDescription,
+    tagLine,
+    posterUrl,
+    path,
   });
 });
 
